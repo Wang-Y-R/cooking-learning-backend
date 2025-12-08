3e9bcf2992d788ad6bbebb25e31760a4e5b41606
xqy <231250140@smail.nju.edu.cn>
2025-12-09 00:48:19 +0800

    fix: typo

--- src/main/java/com/example/cooking/common/constant/WSMessageType.java
+++ src/main/java/com/example/cooking/common/constant/WSMessageType.java
@@ -13,14 +13,12 @@ public class WSMessageType {
 
     public static final String NO_NEXT_STEP = "NO_NEXT_STEP";
 
-    public static final String START_BLOCKABLE  = "START_BLOCKABLE";
+    public static final String START_BLOCKABLE = "START_BLOCKABLE";
 
-    public static final String  BLOCK_FINISHED  = " BLOCK_FINISHED";
+    public static final String BLOCK_FINISHED = "BLOCK_FINISHED";
 
     public static final String HEARTBEAT = "HEARTBEAT";
 
-
     public static final String BIND = "BIND";
-
 
 }
